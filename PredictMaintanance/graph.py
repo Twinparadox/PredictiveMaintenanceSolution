e6fd723973d9792115cbd1ed4d41033ab9f7c27c
Twinparadox <nww731@gmail.com>
2019-12-06 10:21:12 +0900

    [Update] index.html
    index.html에 기능 구현 시작
    Ajax 활용해서 자동으로 POST request 기능 구현 필요

--- PredictMaintanance/graph.py
+++ PredictMaintanance/graph.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 
 @app.route('/graph')
 def graph():
-    return render_template('graph.html', update = "None", sensor = "")
+    return render_template('index.html', update = "None", sensor = "")
 
 @app.route('/draw', methods = ['POST', 'GET'])
 def draw():
@@ -35,8 +35,8 @@ def draw():
     value.append(data[i])
     print(value)
     
-    return render_template('graph.html', update = data[i], sensor = value)
+    return render_template('index.html', update = data[i], sensor = value)
 
 if __name__ == '__main__':
     # Flask 서비스 스타트
-    app.run(host='192.168.0.34', port=8080)
+    app.run(host='localhost', port=8080)
